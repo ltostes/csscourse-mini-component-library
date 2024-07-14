dd322af2cc65228cb7e350353ca604ba7b98169f
ltostes <lucasrmtostes@gmail.com>
2024-07-14 12:48:32 -0300

    select exercise go along

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,65 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper
+          style={{ '--size' : 24 + 'px'}}>
+          <Icon 
+            id="chevron-down"
+            size={24}
+            strokeWidth={2}
+            />
+          </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+  
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black}
+  }
+`
+
+const IconWrapper = styled.span`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  right: 10px;
+  height: var(--size);
+  width: var(--size);
+  pointer-events: none;
+`
 
 export default Select;
